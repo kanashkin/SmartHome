710c016675a9268ab9a500e5ce8ffe1223e8f012
Jordan <molongkan@mail.ru>
2023-03-31 17:36:51 +0300

    Ready groups and devices

--- src/js/app.js
+++ src/js/app.js
@@ -7,6 +7,7 @@ import widgetSwitcher from './modules/widgetSwitcher.js';
 document.addEventListener('DOMContentLoaded', () => {
     positions('.promo__visual')
     positions('.apps__visual')
+    positions('.devices__visual')
     widgetMusic('.widget-music-play')
     scenariosChecked('.real-checkbox')
     widgetSwitcher('.app__widgets__block.cloneable', '.widget-switcher.cloneable', '.app__click')
