95c086a6196c359ec5beb3de9fbdd5aeb9eee01c
Alexander <kanashkin_ad@mail.ru>
2023-04-27 00:48:28 +0300

    Ready title scroll

--- src/js/app.js
+++ src/js/app.js
@@ -3,14 +3,14 @@ import positions from './modules/positions.js';
 import widgetMusic from './modules/widgetMusic.js';
 import scenariosChecked from './modules/scenariosChecked.js';
 import accordion from './modules/accordion.js';
-import pageScroll from './modules/pageScroll.js';
+import siteScroll from './modules/siteScroll.js';
 
 document.addEventListener('DOMContentLoaded', () => {
-    positions('.promo__visual')
-    positions('.apps__visual')
-    positions('.devices__visual')
+    positions('.promo-visual')
+    positions('.apps-visual')
+    positions('.devices-visual')
     widgetMusic('.widget-music-play')
     scenariosChecked('.real-checkbox')
     accordion('.accordion-trigger', '.accordion-content')
-    pageScroll('.arrow', '.page-wrapper', '.header__logo', '.button-header')
+    siteScroll('.arrow')
 })
