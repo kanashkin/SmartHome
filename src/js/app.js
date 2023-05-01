f2ed71248413d340269c66349e7ecb0ff802c71e
Alexander <kanashkin_ad@mail.ru>
2023-05-01 12:56:28 +0300

    add animation on main page

--- src/js/app.js
+++ src/js/app.js
@@ -4,6 +4,7 @@ import widgetMusic from './modules/widgetMusic.js';
 import scenariosChecked from './modules/scenariosChecked.js';
 import accordion from './modules/accordion.js';
 import siteScroll from './modules/siteScroll.js';
+import mouseAnimaton from './modules/mouseAnimation.js';
 
 document.addEventListener('DOMContentLoaded', () => {
     positions('.promo-visual')
@@ -13,4 +14,5 @@ document.addEventListener('DOMContentLoaded', () => {
     scenariosChecked('.real-checkbox')
     accordion('.accordion-trigger', '.accordion-content')
     siteScroll('.arrow', '.header__logo','.button-header')
+    mouseAnimaton('.mouse-block', '.promo-visual')
 })
