a94f9e8bbac335a602e88a8ee19e2378238734a7
Alexander <kanashkin_ad@mail.ru>
2023-06-12 18:03:21 +0300

    ready code to groupsAnimation

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,14 @@
 import './modules/isWebp.js';
 import positions from './modules/positions.js';
 import accordion from './modules/accordion.js';
+import groupsAnimation from './modules/groupsAnimation.js';
 import siteScroll from './modules/siteScroll.js';
 
 document.addEventListener('DOMContentLoaded', () => {
+    groupsAnimation('.groups-item', 'groups-item-center', 'groups-item-left', 'groups-item-right')
     positions('.promo-visual')
     positions('.apps-visual')
     positions('.devices-visual')
     accordion('.accordion-trigger', '.accordion__block')
-    siteScroll('.arrow', '.header__logo','.button-header')
+    // siteScroll('.arrow', '.header__logo','.button-header')
 })
